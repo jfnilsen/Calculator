b341336ae0d11c59dd28d461602356ea8e15052e
Jim Frode Nilsen <jimnil93@hotmail.com>
2016-01-08 17:32:09 +0100

    Settings button in place, working exit button, general refactoring of switches

--- app/src/main/java/com/example/jim/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/jim/calculator/CalculatorActivity.java
@@ -1,12 +1,21 @@
 package com.example.jim.calculator;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import org.matheclipse.parser.client.eval.DoubleEvaluator;
+
+import java.lang.reflect.Field;
 
 
 public class CalculatorActivity extends AppCompatActivity {
@@ -16,62 +25,20 @@ public class CalculatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
     public void clickButton(View view) {
         switch (view.getId()) {
-            case R.id.button0:
-                addButtonValueToTextView("0");
-                break;
-            case R.id.button1:
-                addButtonValueToTextView("1");
-                break;
-            case R.id.button2:
-                addButtonValueToTextView("2");
-                break;
-            case R.id.button3:
-                addButtonValueToTextView("3");
-                break;
-            case R.id.button4:
-                addButtonValueToTextView("4");
-                break;
-            case R.id.button5:
-                addButtonValueToTextView("5");
-                break;
-            case R.id.button6:
-                addButtonValueToTextView("6");
-                break;
-            case R.id.button7:
-                addButtonValueToTextView("7");
-                break;
-            case R.id.button8:
-                addButtonValueToTextView("8");
-                break;
-            case R.id.button9:
-                addButtonValueToTextView("9");
-                break;
-            case R.id.buttonMultiply:
-                addButtonValueToTextView("*");
-                break;
-            case R.id.buttonDivide:
-                addButtonValueToTextView("/");
-                break;
-            case R.id.buttonPlus:
-                addButtonValueToTextView("+");
-                break;
-            case R.id.buttonMinus:
-                addButtonValueToTextView("-");
-                break;
-            case R.id.buttonPeriod:
-                addButtonValueToTextView(".");
-                break;
-            case R.id.buttonOpeningParenthesis:
-                addButtonValueToTextView("(");
-                break;
-            case R.id.buttonClosingParenthesis:
-                addButtonValueToTextView(")");
-                break;
             case R.id.buttonEquals:
                 summarize();
                 break;
@@ -81,60 +48,65 @@ public class CalculatorActivity extends AppCompatActivity {
             case R.id.buttonBack:
                 removeLastDigit();
                 break;
+            default:
+                addButtonValueToTextView(((Button) findViewById(view.getId())).getText().toString());
+                break;
         }
     }
 
-    private void removeLastDigit(){
-        TextView textView = ((TextView)findViewById(R.id.textView));
+    private void removeLastDigit() {
+        TextView textView = ((TextView) findViewById(R.id.textView));
         String text = textView.getText().toString();
-        if(textView.getText().length() > 1){
-            textView.setText(text.substring(0, text.length()-1));
-        }else {
+        if (textView.getText().length() > 1) {
+            textView.setText(text.substring(0, text.length() - 1));
+        } else {
             clearTextView();
         }
     }
 
     private void addButtonValueToTextView(String s) {
-        TextView textView = ((TextView)findViewById(R.id.textView));
-        boolean endsWithOperator = textView.getText().toString().endsWith("*") || textView.getText().toString().endsWith("/") ||
-                textView.getText().toString().endsWith("-") || textView.getText().toString().endsWith("+");
-        switch(s){
+        TextView textView = ((TextView) findViewById(R.id.textView));
+
+        switch (s) {
             case "*":
-                if(!endsWithOperator)
-                    textView.setText(textView.getText() + s);
+                textView.setText(textView.getText() + s);
                 break;
             case "/":
-                if(!endsWithOperator)
-                    textView.setText(textView.getText() + s);
+                textView.setText(textView.getText() + s);
                 break;
             case "-":
                 textView.setText(textView.getText() + s);
                 break;
             case "+":
-                if(!endsWithOperator)
-                    textView.setText(textView.getText() + s);
+                textView.setText(textView.getText() + s);
                 break;
             default:
-                if(!textView.getText().toString().equals("0")){
+                if (!textView.getText().toString().equals("0")) {
                     textView.setText(textView.getText() + s);
-                }else {
+                } else {
                     textView.setText(s);
                 }
                 break;
         }
 
     }
-    private void summarize() {
-        TextView textView = ((TextView)findViewById(R.id.textView));
 
-        try{
-            double result = engine.evaluate(((TextView)findViewById(R.id.textView)).getText().toString());
+    private void summarize() {
+        TextView textView = ((TextView) findViewById(R.id.textView));
+
+        try {
+            double result = engine.evaluate(((TextView) findViewById(R.id.textView)).getText().toString());
             textView.setText(result + "");
-        }catch(Exception e) {
-            Toast.makeText(getApplicationContext(),"Invalid operation", Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), R.string.invalid_operation, Toast.LENGTH_LONG).show();
         }
     }
+
     private void clearTextView() {
-        ((TextView)findViewById(R.id.textView)).setText("0");
+        ((TextView) findViewById(R.id.textView)).setText("0");
+    }
+
+    public void exit(MenuItem item) {
+        this.finish();
     }
 }
