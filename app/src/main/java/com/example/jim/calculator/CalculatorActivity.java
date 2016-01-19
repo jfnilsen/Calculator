2bfca6b3f038d1040e9d0cfba3abcdb26f3b3c94
Jim Frode Nilsen <jimnil93@hotmail.com>
2016-01-19 15:56:51 +0100

    Attempt for factorial

--- app/src/main/java/com/example/jim/calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/jim/calculator/CalculatorActivity.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import org.matheclipse.parser.client.eval.ComplexEvaluator;
 import org.matheclipse.parser.client.eval.DoubleEvaluator;
 import org.w3c.dom.Text;
 
@@ -24,7 +25,7 @@ import java.lang.reflect.Field;
 
 
 public class CalculatorActivity extends AppCompatActivity {
-    DoubleEvaluator engine = new DoubleEvaluator();
+    ComplexEvaluator engine = new ComplexEvaluator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +56,7 @@ public class CalculatorActivity extends AppCompatActivity {
             case R.id.science_calc_setting:
                 Toast.makeText(getApplicationContext(),R.string.science_calculator, Toast.LENGTH_LONG).show();
                 setContentView(R.layout.activity_scientific_calculator);
-                setSupportActionBar((Toolbar)findViewById(R.id.my_toolbar));
+                setSupportActionBar((Toolbar) findViewById(R.id.my_toolbar));
                 return true;
 
             case R.id.normal_calc_setting:
@@ -114,8 +115,18 @@ public class CalculatorActivity extends AppCompatActivity {
                 break;
             case R.id.buttonLog:
                 addButtonValueToTextView("Log[");
+                break;
             case R.id.buttonX2:
                 addButtonValueToTextView("^2");
+                break;
+            case R.id.buttonSqrt:
+                addButtonValueToTextView("Sqrt[");
+                break;
+            case R.id.buttonExp:
+                addButtonValueToTextView("Exp[");
+                break;
+            case R.id.buttonFactorial:
+                addButtonValueToTextView("!");
                 break;
             default:
                 addButtonValueToTextView(((Button) findViewById(view.getId())).getText().toString());
@@ -160,13 +171,34 @@ public class CalculatorActivity extends AppCompatActivity {
 
     }
 
-    private void summarize() {
-        TextView textView = ((TextView) findViewById(R.id.result_textView));
+    private double factorial(String numberAsString){
+        int n = Integer.parseInt(numberAsString);
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 
+
+    private void summarize() {
+        TextView resultView = ((TextView) findViewById(R.id.result_textView));
+        String expression = ((TextView) findViewById(R.id.textView)).getText().toString();
+        String[] expressionParts = expression.split("!");
+
+        if(expressionParts.length > 1){
+            for(int i = 0; i < (expressionParts.length - 1); i++){
+                String[] subParts = expressionParts[i].split("[+[/]-*]");
+                double result = factorial(subParts[subParts.length-1]);
+                System.out.println();
+            }
+        }
         try {
-            double result = engine.evaluate(((TextView) findViewById(R.id.textView)).getText().toString());
-            textView.setText(String.valueOf(result));
+            String result = ComplexEvaluator.toString(engine.evaluate(expression));
+            resultView.setText(String.valueOf(result));
         } catch (Exception e) {
+            Log.d("Errorlog" ,e.getMessage());
+            e.printStackTrace();
             Toast.makeText(getApplicationContext(), R.string.invalid_operation, Toast.LENGTH_LONG).show();
         }
     }
